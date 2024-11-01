cfc73c3a1d7172c79a55a153098ff4cdb0491da6
saimiiqbal7 <saimiiqbal7@gmail.com>
2024-11-01 14:27:02 -0230

    first commit

---
+++ url.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+class Url:
+
+    def __init__(self):
+
+        self.urlMapping = {}
+
+    def uniqueCode(self):
+
+        all = string.ascii_letters + string.digits
+        length = len(all)
+        code = ""
+        for i in range(10):
+            code += all[random.randint(0,length-1)]
+        
+        return code
+
+url = Url()
+code = url.uniqueCode()
+print(code)
+
+
